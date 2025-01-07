90a2a520fa619284ac3ee2a825c4b5e313f96020
jinmw <jinmw@rd.netease.com>
2025-01-07 15:12:02 +0800

    feat: 完善游戏

--- assets/Script/GameManager.ts
+++ assets/Script/GameManager.ts
@@ -1,26 +1,120 @@
 import { _decorator, CCInteger, Component, instantiate, Label, math, Node, Prefab, Vec3 } from 'cc';
-import { BLOCK_SIZE } from './PlayerController';
+import { BLOCK_SIZE, PlayerController } from './PlayerController';
 const { ccclass, property } = _decorator;
 
 
 enum BlockType{
-    BT_NONE,
-    BT_STONE,
+    BT_NONE, // 空地块
+    BT_STONE, //  石头地块
 };
+
+/**
+ * GS_INIT 时我们先显示 StartMenu、创建地图以及重设角色的为和状态并禁用角色输入
+ * GS_PLAYING：在状态下隐藏 StartMenu、重设计步器的数值以及启用用户输入
+ */
+
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
+
 
 @ccclass('GameManager')
 export class GameManager extends Component {
 
+    // public 这个会外显给gameManager,可以用来绑定对应的元素
     @property({type: Prefab})
     public boxPrefab: Prefab|null = null;
     @property({type: CCInteger})
-    public roadLength: number = 50;
-    private _road: BlockType[] = [];
+    public roadLength: number = 50; // 路长度
+    private _road: BlockType[] = []; // 路的数组
+
+    @property({ type: Node }) // 开始菜单
+    public startMenu: Node | null = null;
+    @property({ type: PlayerController }) // 角色控制器(角色实例本身)
+    public playerCtrl: PlayerController | null = null;
+    @property({type: Label})  // 屏幕右上角的步数显示
+    public stepsLabel: Label|null = null;
 
     start() {
-        this.generateRoad()
+        this.setCurState(GameState.GS_INIT); // 第一初始化要在 start 里面调用
+        this.playerCtrl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
     }
 
+    // 初始化状态
+    init() {
+        if (this.startMenu) {
+            // 开启菜单
+            this.startMenu.active = true;
+        }
+
+        // 生成随机道路
+        this.generateRoad();
+
+        if (this.playerCtrl) {
+            // 关闭鼠标点击事件
+            this.playerCtrl.setInputActive(false);
+            // 设置为原点
+            this.playerCtrl.node.setPosition(Vec3.ZERO);
+            this.playerCtrl.reset();
+        }
+    }
+
+    // 状态流转
+    setCurState(value: GameState) {
+        switch (value) {
+            case GameState.GS_INIT:
+                this.init();
+                break;
+            case GameState.GS_PLAYING:
+                // 隐藏开始菜单
+                if (this.startMenu) {
+                    this.startMenu.active = false;
+                }
+
+                if (this.stepsLabel) {
+                    this.stepsLabel.string = '步数：0';   // 将步数重置为0
+                }
+
+                //直接设置active会直接开始监听鼠标事件，导致点play的那下也算，做了一下延迟处理
+                setTimeout(() => {
+                    if (this.playerCtrl) {
+                        this.playerCtrl.setInputActive(true);
+                    }
+                }, 0.1);
+                break;
+            case GameState.GS_END:
+                break;
+        }
+    }
+
+    // 生成下方的道路
+    generateRoad() {
+        this.node.removeAllChildren();
+
+        this._road = [];
+        // 起点有石头
+        this._road.push(BlockType.BT_STONE);
+
+        for (let i = 1; i < this.roadLength; i++) {
+            if (this._road[i - 1] === BlockType.BT_NONE) { // 如果上一个是空地块，那么这个地块就是石头
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2)); // 如果上一块有石头，随机生成一个有石头的地块|空地块
+            }
+        }
+
+        for (let j = 0; j < this._road.length; j++) {
+            let block: Node | null = this.spawnBlockByType(this._road[j]);
+            if (block) {
+                this.node.addChild(block);
+                block.setPosition(j * BLOCK_SIZE, 0, 0);
+            }
+        }
+    }
+
+    // 通过 spawnBlockByType 来生成新的方块并将它通过 setPosition 方法放置到合适的位置。
     spawnBlockByType(type: BlockType) {
         if (!this.boxPrefab) {
             return null;
@@ -32,32 +126,30 @@ export class GameManager extends Component {
                 block = instantiate(this.boxPrefab);
                 break;
         }
-
         return block;
     }
 
-    generateRoad() {
+    // 点击开始玩
+    onStartButtonClicked() {
+        this.setCurState(GameState.GS_PLAYING);
+    }
 
-        this.node.removeAllChildren();
+    // 增加一个用于判定角色是否跳跃到坑或者跳完所有地块的方法
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength) {
+            if (this._road[moveIndex] == BlockType.BT_NONE) {   //跳到了空方块上
 
-        this._road = [];
-        // startPos
-        this._road.push(BlockType.BT_STONE);
-
-        for (let i = 1; i < this.roadLength; i++) {
-            if (this._road[i - 1] === BlockType.BT_NONE) {
-                this._road.push(BlockType.BT_STONE);
-            } else {
-                this._road.push(Math.floor(Math.random() * 2));
+                this.setCurState(GameState.GS_INIT)
             }
+        } else {    // 跳过了最大长度
+            this.setCurState(GameState.GS_INIT);
         }
+    }
 
-        for (let j = 0; j < this._road.length; j++) {
-            let block: Node | null = this.spawnBlockByType(this._road[j]);
-            if (block) {
-                this.node.addChild(block);
-                block.setPosition(j * BLOCK_SIZE, 0, 0);
-            }
+    onPlayerJumpEnd(moveIndex: number) {
+        if (this.stepsLabel) {
+            this.stepsLabel.string = '步数：' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
         }
+        this.checkResult(moveIndex);
     }
 }
--- assets/Script/PlayerController.ts
+++ assets/Script/PlayerController.ts
@@ -1,10 +1,10 @@
 import { _decorator, Component, Vec3, EventMouse, input, Input, Animation } from "cc";
 const { ccclass, property } = _decorator;
 
-export const BLOCK_SIZE = 40; // 添加一个放大比
+export const BLOCK_SIZE = 40;
 
-@ccclass('NewComponent')
-export class NewComponent extends Component {
+@ccclass("PlayerController")
+export class PlayerController extends Component {
     private _startJump: boolean = false; // 是否开始跳跃
     private _jumpStep: number = 0; // 跳跃步数
     private _curJumpTime: number = 0; // 当前的跳跃时间
@@ -13,43 +13,55 @@ export class NewComponent extends Component {
     private _curPos: Vec3 = new Vec3(); // 当前的位置
     private _deltaPos: Vec3 = new Vec3(0, 0, 0); // 位移
     private _targetPos: Vec3 = new Vec3(); // 目标位置
+    private _curMoveIndex: number = 0; // 现在移动了多少步
 
-    // 动画绑定位置，声明后会在目标节点出现绑定动画插槽位置
     @property(Animation)
     BodyAnim:Animation = null;
 
-
-    start() {
-        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this)
+    start () {
+        //input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
     }
 
-    onMouseUp(event: EventMouse) {
-        if (event.getButton() === 0) { // 左键按下的时候为0
-            this.jumpByStep(1);
-        } else if (event.getButton() === 2) { // 右键为2
-            this.jumpByStep(2);
+    // 是否监听鼠标抬起事件
+    setInputActive(active: boolean) {
+        if (active) {
+            input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
         }
     }
 
-    // jumpByStep(step: number) {
-    //     if (this._startJump) {
-    //         return;
-    //     }
-    //     this._startJump = true;  // 标记开始跳跃
-    //     this._jumpStep = step; // 跳跃的步数 1 或者 2
-    //     this._curJumpTime = 0; // 重置开始跳跃的时间
-    //     this._curJumpSpeed = this._jumpStep * BLOCK_SIZE/ this._jumpTime;
-    //     this.node.getPosition(this._curPos); // 获取角色当前的位置
-    //     Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep* BLOCK_SIZE, 0, 0));  // 计算出目标位置
-    // }
+    // 重置状态
+    reset() {
+        // 当前操作部署
+        this._curMoveIndex = 0;
+        this.node.getPosition(this._curPos);
+        this._targetPos.set(0,0,0);
+    }
 
+    // 点击跳跃，左键挑一步，右键跳两步
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+
+    }
+
+    // 跳跃
     jumpByStep(step: number) {
         if (this._startJump) {
             return;
         }
-        this._startJump = true; // 开始跳跃
-        this._jumpStep = step; // 跳跃的步长
-        this._curJumpTime = 0; //
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+
+        const clipName = step == 1 ? 'oneStep' : 'twoStep';
+        const state = this.BodyAnim.getState(clipName);
+        this._jumpTime = state.duration;
+
         this._curJumpSpeed = this._jumpStep * BLOCK_SIZE/ this._jumpTime;
         this.node.getPosition(this._curPos);
         Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep* BLOCK_SIZE, 0, 0));
@@ -61,24 +73,31 @@ export class NewComponent extends Component {
                 this.BodyAnim.play('twoStep');
             }
         }
+
+        this._curMoveIndex += step;
     }
 
+
+    onOnceJumpEnd() {
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+
+    // 更新位置
     update (deltaTime: number) {
         if (this._startJump) {
-            this._curJumpTime += deltaTime; // 累计总的跳跃时间
-            if (this._curJumpTime > this._jumpTime) { // 当跳跃时间是否结束
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
                 // end
-                this.node.setPosition(this._targetPos); // 强制位置到终点
-                this._startJump = false;               // 清理跳跃标记
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
             } else {
                 // tween
                 this.node.getPosition(this._curPos);
-                this._deltaPos.x = this._curJumpSpeed * deltaTime; //每一帧根据速度和时间计算位移
-                Vec3.add(this._curPos, this._curPos, this._deltaPos); // 应用这个位移
-                this.node.setPosition(this._curPos); // 将位移设置给角色
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
             }
         }
     }
 }
-
-
